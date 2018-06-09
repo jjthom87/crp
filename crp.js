7966d77e5c36c87b4eeba0b803ce2336885c3fc3
Jared Thomas <jjthom87@yahoo.com>
2018-06-09 12:50:27 -0400

    adding

--- crp.js
+++ crp.js
@@ -4,14 +4,43 @@ var argOne = process.argv[2];
 var argTwo = process.argv[3];
 const { exec } = require('child_process');
 
+console.log(process.argv)
+
 if(!argOne){
-	console.log("Please enter folder name")
+	console.log("--project-name <Enter Project Name>")
 } else if (argOne === "--help"){
-	console.log("Please enter a folder name as your second argument")
-} else {
-	exec('mkdir ' + argOne, (err, stdout, stderr) => {
-	  if (err) {
-	    return;
-	  }
+	console.log("--project-name <Enter Project Name>")
+} else if (argOne === "--project-name" && argTwo) {
+	exec('mkdir ' + argTwo, (err, stdout, stderr) => {
+		if (err) {
+		    return;
+		}
+		exec('mkdir ' + argTwo + "/server", (err, stdout, stderr) => {
+			if (err) {
+			    return;
+			}
+			exec('mkdir ' + argTwo + "/server/controller", (err, stdout, stderr) => {
+				if (err) {
+				    return;
+				}
+				exec('touch ' + argTwo + "/server/controller/route_controller.js", (err, stdout, stderr) => {
+					if (err) {
+					    return;
+					}
+				});
+			});
+			exec('touch ' + argTwo + "/server/server.js", (err, stdout, stderr) => {
+				if (err) {
+				    return;
+				}
+			});
+		});
+		exec('mkdir ' + argTwo + "/client", (err, stdout, stderr) => {
+			if (err) {
+			    return;
+			}
+		});
 	});
+} else if (argOne === "--project-name" && !argTwo){
+	console.log("--project-name <Enter Project Name>")
 }
