2ce713e9f5c8312a51d19618b6bcc8dfebb76578
Jared Thomas <jjthom87@yahoo.com>
2018-06-16 02:27:53 -0400

    adding

--- crp.js
+++ crp.js
@@ -3,44 +3,33 @@
 var argOne = process.argv[2];
 var argTwo = process.argv[3];
 const { exec } = require('child_process');
-
-console.log(process.argv)
+const chalk = require('chalk');
+const log = console.log;
 
 if(!argOne){
 	console.log("--project-name <Enter Project Name>")
 } else if (argOne === "--help"){
 	console.log("--project-name <Enter Project Name>")
-} else if (argOne === "--project-name" && argTwo) {
+} else if (process.argv.indexOf("--project-name") > -1 && process.argv[process.argv.indexOf("--project-name") + 1]) {
 	exec('mkdir ' + argTwo, (err, stdout, stderr) => {
 		if (err) {
 		    return;
 		}
-		exec('mkdir ' + argTwo + "/server", (err, stdout, stderr) => {
+		exec('cp -r ./lesson/* ./' + argTwo + "/", (err, stdout, stderr) => {
 			if (err) {
 			    return;
 			}
-			exec('mkdir ' + argTwo + "/server/controller", (err, stdout, stderr) => {
+			log(chalk.red.bold('Loading Node Modules'));
+			exec('cd ./' + argTwo + "/; npm i;", (err, stdout, stderr) => {
 				if (err) {
 				    return;
 				}
-				exec('touch ' + argTwo + "/server/controller/route_controller.js", (err, stdout, stderr) => {
-					if (err) {
-					    return;
-					}
-				});
-			});
-			exec('touch ' + argTwo + "/server/server.js", (err, stdout, stderr) => {
-				if (err) {
-				    return;
+				if(stdout){
+					log(chalk.blue.bold('Node Modules Loaded'))
 				}
 			});
-		});
-		exec('mkdir ' + argTwo + "/client", (err, stdout, stderr) => {
-			if (err) {
-			    return;
-			}
 		});
 	});
-} else if (argOne === "--project-name" && !argTwo){
+} else if (process.argv.indexOf("--project-name") > -1 && !argTwo){
 	console.log("--project-name <Enter Project Name>")
 }
